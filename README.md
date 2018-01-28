1750953a112b3b36ebc7cec376bf278e8eddd1a0
Rowan Thorpe <rowan@rowanthorpe.com>
2018-01-28 23:43:07 +0200

    Add build info to README,improve authorship info

--- README.md
+++ README.md
@@ -20,14 +20,59 @@ the top of the file for copyright information.
 I started making these changes because I came across an error, and
 despite the various fixes I made I ran out of time to diagnose that
 original error, and as it is not a showstopper for me anyway I am just
-leaving it in the below FIXME.
+leaving it in the FIXME at the bottom of this README.
 
 Any bug-reports of *the local changes I have made* can be
 [reported to me](mailto:rowan@rowanthorpe.com) but anything to do with
 the upstream tool itself should still be reported there.
+
+Build instructions
+------------------
+
+A [CMake](https://cmake.org) file has been contributed, so if you are in a hurry (or lazy, not
+confident compiling, or whatever), if CMake is installed you can do the following to build a
+standard non-optimized executable and install it to your system's default custom-compilation
+bin-dir (of course CMake must be installed):
+
+```sh
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+```
+
+`makeheaders` is plain ansi c, and doesn't depend on anything outside the standard library, so
+should be buildable with any command along the lines of:
+
+```sh
+${COMPILER} ${WARNINGS_FLAGS} ${OPTIMIZATION_FLAGS} -o makeheaders makeheaders.c
+```
+
+Read the comments near the top of `makeheaders.c` to see how I build it with `gcc` in
+mem-testing mode, stack-checking mode, and final mode, and for `valgrind` commandlines for
+testing the resulting executable.
+
+License
+-------
+
+Makeheaders is under the "2-Clause License" ("FreeBSD License"). See the license-header at the
+top of the `makeheaders.c` file for details.
 
 FIXME
 -----
 
 * CompleteForwardDeclarations does `abort()` when running `./makeheaders -local makeheaders.c`
   (seems to be an issue on static functions with no preceding declaration)
+
+Authors
+-------
+
+Upstream author:
+ [D. Richard Hipp](http://www.hwaci.com/drh/index.html)
+
+Maintainer of this repo:
+ [Rowan Thorpe](https://github.com/rowanthorpe)
+
+Contributions:
+ [xoviat](https://github.com/xoviat) - CMake file
--- makeheaders.c
+++ makeheaders.c
@@ -1,4 +1,45 @@
 /*
+** This program has had fixes and tweaks on top of the original program.
+** See the README file for authorship.
+*/
+
+/*
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the Simplified BSD License (also
+** known as the "2-Clause License" or "FreeBSD License".)
+**
+** Copyright 1993 D. Richard Hipp. All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or
+** without modification, are permitted provided that the following
+** conditions are met:
+**
+**   1. Redistributions of source code must retain the above copyright
+**      notice, this list of conditions and the following disclaimer.
+**
+**   2. Redistributions in binary form must reproduce the above copyright
+**      notice, this list of conditions and the following disclaimer in
+**      the documentation and/or other materials provided with the
+**      distribution.
+**
+** This software is provided "as is" and any express or implied warranties,
+** including, but not limited to, the implied warranties of merchantability
+** and fitness for a particular purpose are disclaimed.  In no event shall
+** the author or contributors be liable for any direct, indirect, incidental,
+** special, exemplary, or consequential damages (including, but not limited
+** to, procurement of substitute goods or services; loss of use, data or
+** profits; or business interruption) however caused and on any theory of
+** liability, whether in contract, strict liability, or tort (including
+** negligence or otherwise) arising in any way out of the use of this
+** software, even if advised of the possibility of such damage.
+**
+** This program is distributed in the hope that it will be useful,
+** but without any warranty; without even the implied warranty of
+** merchantability or fitness for a particular purpose.
+** appropriate header files.
+*/
+
+/*
 
  COMPILATION:
 
@@ -42,41 +83,7 @@ static const char ident[] = "@(#) $Header: /cvstrac/cvstrac/makeheaders.c,v 1.4 
 #if defined(__clang__) || (defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7)))
 #pragma GCC diagnostic pop
 #endif
-/*
-** This program is free software; you can redistribute it and/or
-** modify it under the terms of the Simplified BSD License (also
-** known as the "2-Clause License" or "FreeBSD License".)
-**
-** Copyright 1993 D. Richard Hipp. All rights reserved.
-**
-** Redistribution and use in source and binary forms, with or
-** without modification, are permitted provided that the following
-** conditions are met:
-**
-**   1. Redistributions of source code must retain the above copyright
-**      notice, this list of conditions and the following disclaimer.
-**
-**   2. Redistributions in binary form must reproduce the above copyright
-**      notice, this list of conditions and the following disclaimer in
-**      the documentation and/or other materials provided with the
-**      distribution.
-**
-** This software is provided "as is" and any express or implied warranties,
-** including, but not limited to, the implied warranties of merchantability
-** and fitness for a particular purpose are disclaimed.  In no event shall
-** the author or contributors be liable for any direct, indirect, incidental,
-** special, exemplary, or consequential damages (including, but not limited
-** to, procurement of substitute goods or services; loss of use, data or
-** profits; or business interruption) however caused and on any theory of
-** liability, whether in contract, strict liability, or tort (including
-** negligence or otherwise) arising in any way out of the use of this
-** software, even if advised of the possibility of such damage.
-**
-** This program is distributed in the hope that it will be useful,
-** but without any warranty; without even the implied warranty of
-** merchantability or fitness for a particular purpose.
-** appropriate header files.
-*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
